27741177d486e2b81da5dcdb15a1cd9b624504a3
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2023-07-20 15:43:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-img = "";
 status1 = "";
 objects = [];
 
 
 function preload() {
-img = loadImage("dog_cat.jpg");
+
 }
 
 function setup() {
-canvas = createCanvas(600, 400);
+canvas = createCanvas(300, 300);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
 objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
 function modelLoaded() {
     console.log("Model is Loaded!");
     status1 = true;
-    objectDetector.detect(img, gotResult);  
+    objectDetector.detect(video, gotResult);  
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
@@ -29,15 +31,19 @@ function gotResult(error, results) {
 }
 
 function draw() {
-image(img, 0, 0, 600, 400);
+image(video, 0, 0, 600, 400);
 if (status1 != "") {
+    r = random(255);
+    g = random(255);
+    b = random(255);
     for( i = 0; i < objects.length; i++) {
         document.getElementById("status").innerHTML = "Status: Objects Detected";
-        fill("#FF0000");
+        document.getElementById("number-of-objects").innerHTML = "No. of objects detected :"+ objects.length;
+        fill(r,g,b);
         percent = floor(objects[i].confidence * 100);
         text(objects[i].label + " " + percent + "%", objects[i].x + 30, objects[i].y + 30);
         noFill();
-        stroke("#FF0000");
+        stroke(r,g,b);
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
 }
